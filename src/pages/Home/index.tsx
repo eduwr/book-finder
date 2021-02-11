826c73ccce37f1d2670e02b5f4badfc46c6e9a00
eduwr <eduwric@gmail.com>
2021-02-11 00:30:36 -0300

    feat: table components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Header, Divider } from "../../components";
+import { BooksTable } from "../../components/organisms/BooksTable";
 import { YearSection } from "../../components/organisms/YearSection";
-
+import { HomeContainer } from "./styles";
 export const Home = () => (
-  <>
+  <HomeContainer>
     <Header />
     <Divider />
     <YearSection />
     <Divider />
-  </>
+    <BooksTable />
+  </HomeContainer>
 );
